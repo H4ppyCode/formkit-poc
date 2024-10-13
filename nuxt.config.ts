594e045bfbedc384e86d50bf7fffe2ca194feb86
Pacôme <dhiser.pacome@gmail.com>
2024-10-13 17:57:49 +0200

    feat(formkit): add formkit.config.ts and update app.vue with new character form (#main)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss'],
+
+  formkit: {
+    // Experimental support for auto loading (see note):
+    configFile: './formkit.config.ts',
+
+    autoImport: true
+  },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
